52e8d9da7b9df3b5b95da536f13c9ac2958056de
deedata <deidra108@gmail.com>
2021-11-23 13:46:22 -0700

    create seed data for LeaveTypes

---
+++ Packt_HRLeaveManagement/Persistence.ClassLibrary/Configurations/LeaveAllocationConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Domain.ClassLibrary;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.ClassLibrary.Configurations
+{
+    public class LeaveAllocationConfiguration : IEntityTypeConfiguration<LeaveAllocation>
+    {
+        public void Configure(EntityTypeBuilder<LeaveAllocation> builder)
+        {
+            
+        }
+    }
+}
---
+++ Packt_HRLeaveManagement/Persistence.ClassLibrary/Configurations/LeaveRequestConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Application.ClassLibrary.DTOs.LeaveRequest;
+using Domain.ClassLibrary;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.ClassLibrary.Configurations
+{
+    public class LeaveRequestConfiguration : IEntityTypeConfiguration<LeaveRequest>
+    {
+        //implement interface
+        public void Configure(EntityTypeBuilder<LeaveRequest> builder)
+        {
+        }
+    }
+}
